5f4b3bee71655acef576aa4f545cc99cf49b1349
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-10 09:36:26 +0545

    connecting the update status url

--- crm/urls.py
+++ crm/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.create_lead, name='create_lead'),
     path('success/', views.lead_success, name='lead_success'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('update-status/<int:lead_id>/', views.update_lead_status, name='update_lead_status'),
+
 ]
