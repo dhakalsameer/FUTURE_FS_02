0770111d5c8c94baeeec57eabf3f8d8cee3caabd
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-10 09:35:49 +0545

    updating the leat status

--- crm/views.py
+++ crm/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Lead
 from .forms import LeadForm
+from django.shortcuts import get_object_or_404
 
 
 def create_lead(request):
@@ -27,3 +28,13 @@ def lead_success(request):
 def dashboard(request):
     leads = Lead.objects.all().order_by('-created_at')
     return render(request, 'crm/dashboard.html', {'leads': leads})
+
+@login_required
+def update_lead_status(request, lead_id):
+    lead = get_object_or_404(Lead, id=lead_id)
+
+    if request.method == 'POST':
+        lead.status = request.POST.get('status')
+        lead.save()
+
+    return redirect('dashboard')
