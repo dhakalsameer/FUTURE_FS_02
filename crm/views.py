75065efbdd979e04c7382f10db93f47cb19f005a
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-10 09:22:06 +0545

    creating the viewspy logic

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Lead
+from .forms import LeadForm
 
-# Create your views here.
+
+def create_lead(request):
+    """
+    Public contact form
+    """
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lead_success')
+    else:
+        form = LeadForm()
+
+    return render(request, 'crm/create_lead.html', {'form': form})
+
+
+def lead_success(request):
+    return render(request, 'crm/lead_success.html')
+
+
+@login_required
+def dashboard(request):
+    leads = Lead.objects.all().order_by('-created_at')
+    return render(request, 'crm/dashboard.html', {'leads': leads})
